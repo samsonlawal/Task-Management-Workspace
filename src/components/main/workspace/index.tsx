a98c6606c4dc117e42fd063a44cb0f8f1ae5b796
deji <lawal4211@gmail.com>
2025-02-06 13:51:28 +0100

    criteria header

--- src/components/main/workspace/index.tsx
+++ src/components/main/workspace/index.tsx
@@ -1,10 +1,12 @@
+import Criteria from "@/components/reusables/Criteria";
 import WSHeading from "@/components/reusables/WSHeading";
 import React from "react";
 
 export default function Workspace() {
   return (
-    <div className="flex flex-1 flex-col rounded-bl-[15px] rounded-tl-[15px] px-[18px] py-[31px] font-madei">
+    <div className="flex flex-1 flex-col gap-8 rounded-bl-[15px] rounded-tl-[15px] px-[18px] py-[31px] font-madei">
       <WSHeading />
+      <Criteria />
     </div>
   );
 }
---
+++ src/components/reusables/Criteria/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Criteria() {
+  return (
+    <div className="flex h-[522px] w-[312px] flex-col items-center justify-center bg-[#F2F2F2]">
+      <div className="flex h-[51px] w-full flex-row items-center justify-between bg-[#DBDBDB] px-[16px]">
+        <p>
+          TO DO
+          <span className="text-[#707070]"> - 6</span>
+        </p>
+
+        <img src="icons/expand.svg" alt="" />
+      </div>
+    </div>
+  );
+}
